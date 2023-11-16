a0a4d8a2e222b8fca10e3648bfe9c13402667597
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-11-17 00:32:01 +0200

    Add scroll on the main page

--- src/components/pages/Vehicles.jsx
+++ src/components/pages/Vehicles.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { getVehicles } from '../../redux/cars/vehicles';
 import './Vehicle.css'
+import { register } from 'swiper/element/bundle';
+register();
 
 const Vehicles = () => {
   const dispatch = useDispatch();
@@ -30,20 +32,24 @@ const Vehicles = () => {
     <div className="vehiclesDiv">
       <h1>CARS ON SHOWROOM </h1>
       <h2>Please select a Car</h2>
-      <div className="vehicles">
-        {vehicles.vehicles.map((veh) => ( // Changed 'vehicle' to 'vehicles'
+      <swiper-container className="vehicles" navigation="true" pagination="true" scrollbar="true" slides-per-view="1" speed="500" loop="true" css-mode="true">
+    
+        {vehicles.vehicles.map((veh) => ( 
+          <swiper-slide>
           <NavLink
-            key={veh.id} // Added a unique key prop
-            to={`/details/${veh.id}`}
-          >
-            <div className="vehicleDiv" onClick={getId(veh.id)}>
-              <img src={veh.img} className="vehicleImg" alt="" />
-              <h3>{veh.name}</h3>
-              <p>{veh.description}</p>
-            </div>
-          </NavLink>
+                      key={veh.id} // Added a unique key prop
+                      to={`/details/${veh.id}`}
+                    >
+                      <div className="vehicleDiv" onClick={getId(veh.id)}>
+                        <img src={veh.img} className="vehicleImg" alt="" />
+                        <h3>{veh.name}</h3>
+                        <p>{veh.description}</p>
+                      </div>
+                    </NavLink>
+          </swiper-slide>
         ))}
-      </div>
+      
+      </swiper-container>
     </div>
   );
 };
