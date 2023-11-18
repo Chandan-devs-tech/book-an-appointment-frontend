45e96a1c50a943de2b70b05b518d34d067a7067c
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-18 20:31:23 +0530

    Modify src/components/pages/Vehicles.jsx

--- src/components/pages/Vehicles.jsx
+++ src/components/pages/Vehicles.jsx
@@ -1,55 +1,44 @@
-/* eslint-disable */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { getVehicles } from '../../redux/cars/vehicles';
-import './Vehicle.css'
 import { register } from 'swiper/element/bundle';
+import { Carousel } from 'react-bootstrap';
+import { fetchCars } from '../../redux/cars/CarsSlice';
+import SidebarComponents from '../sideBar/sideBarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 register();
 
 const Vehicles = () => {
   const dispatch = useDispatch();
-  const vehicles = useSelector((state) => state.vehicles); // Changed 'vehicle' to 'vehicles'
+  const vehicles = useSelector((state) => state.car.cars);
   useEffect(() => {
-    dispatch(getVehicles());
-  }, [dispatch]); // Added 'dispatch' to the dependency array
-
-  const [get, setGet] = useState(null);
-
-  const getId = (id) => async () => { // Corrected the getId function
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/v1/cars/${id}`,
-      );
-      const data = await response.json();
-      setGet(data);
-    } catch (error) {
-      console.error('Error fetching vehicle details:', error);
+    if (vehicles.length === 0) {
+      dispatch(fetchCars());
     }
-  };
+  }, [dispatch]);
 
   return (
-    <div className="vehiclesDiv">
-      <h1>CARS ON SHOWROOM </h1>
-      <h2>Please select a Car</h2>
-      <swiper-container className="vehicles" navigation="true" pagination="true" scrollbar="true" slides-per-view="1" speed="500" loop="true" css-mode="true">
-    
-        {vehicles.vehicles.map((veh) => ( 
-          <swiper-slide>
-          <NavLink
-                      key={veh.id} // Added a unique key prop
-                      to={`/details/${veh.id}`}
-                    >
-                      <div className="vehicleDiv" onClick={getId(veh.id)}>
-                        <img src={veh.img} className="vehicleImg" alt="" />
-                        <h3>{veh.name}</h3>
-                        <p>{veh.description}</p>
-                      </div>
-                    </NavLink>
-          </swiper-slide>
-        ))}
-      
-      </swiper-container>
+    <div className="main-page">
+      <SidebarComponents />
+      <div className="vehiclesDiv">
+        <h1>CARS ON SHOWROOM</h1>
+        <h2>Please select a Car</h2>
+        <Carousel interval={null} indicators={false}>
+          {vehicles.map((veh) => (
+            <Carousel.Item key={veh.id}>
+              <Link className="details-link" to={`/details/${veh.id}`}>
+                <div className="vehicleDiv">
+                  <img src={veh.img} className="vehicleImg" alt="" />
+                  <h3 className="car-name">{veh.name}</h3>
+                  <p className="car-star">******************************</p>
+                  <p className="car-description">{veh.description}</p>
+                </div>
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 };
