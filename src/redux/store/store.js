f1420db3a5737806dead03bbef8892277f2d9e74
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-11-18 20:52:16 +0530

    Modify src/redux/store/store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
-
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
@@ -9,20 +7,20 @@ import {
   PAUSE,
   PERSIST,
   PURGE,
-  REGISTER
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import userSlice from '../user/user'
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userSlice from '../user/user';
 // import vehicleSlice from '../cars/vehicles';
-import carsReducer from "../cars/CarsSlice";
+import carsReducer from '../cars/CarsSlice';
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, userSlice)
+const persistedReducer = persistReducer(persistConfig, userSlice);
 
 export const store = configureStore({
   reducer: {
@@ -30,12 +28,11 @@ export const store = configureStore({
     // vehicles: vehicleSlice,
     car: carsReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
