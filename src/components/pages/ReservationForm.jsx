31181f5d5e48d865bf7b306008dc4be1ee0329ef
mariegrace31 <mariegracebmg@gmail.com>
2023-11-20 16:11:57 +0200

    Add reservation form

---
+++ src/components/pages/ReservationForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+function ReservationForm({ onSubmit }) {
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    car: '',
+    city: '',
+    date: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="username" value={formData.username} readOnly />
+      <input type="text" name="car" value={formData.car} readOnly />
+      <input type="text" name="city" value={formData.city} onChange={handleInputChange} placeholder="City" />
+      <input type="date" name="date" value={formData.date} onChange={handleInputChange} placeholder="Date" />
+      <button type="submit">Add Reservation</button>
+    </form>
+  );
+}
+
+export default ReservationForm;
