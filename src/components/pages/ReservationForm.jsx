cf63727bd25339d8853afababe80a7f9fbc7efe7
mariegrace31 <mariegracebmg@gmail.com>
2023-11-22 01:00:08 +0200

    Style the reservation form

--- src/components/pages/ReservationForm.jsx
+++ src/components/pages/ReservationForm.jsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import { createReservations } from '../../redux/reservation/reservationSlice';
+import './reservationForm.css';
 
 function ReservationForm({ car }) {
   const currentUser = useSelector((state) => state.user.currentUser);
@@ -35,28 +36,30 @@ function ReservationForm({ car }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Reserve this car</h2>
-      <input type="text" name="user_id" value={currentUser.name} readOnly />
-      { car ? <input type="text" name="car_id" value={car.name} readOnly />
-        : (
-          <select name="car_id" value={formData.car} onChange={handleInputChange}>
-            <option disabled value="Select a car">Select a Car</option>
-            {cars?.map((car) => (
-              <option key={car.id} value={car.id}>{car.name}</option>
-            ))}
-          </select>
-        )}
-      <select name="city" value={formData.city} onChange={handleInputChange}>
-        <option value="">Select City</option>
-        <option value="San Francisco">San Francisco</option>
-        <option value="Los Angeles">Los Angeles</option>
-        <option value="New York">New York</option>
-        <option value="Chicago">Chicago</option>
-      </select>
-      <input type="date" name="date" value={formData.date} onChange={handleInputChange} placeholder="Date" />
-      <button type="submit">Add Reservation</button>
-    </form>
+    <div className="popup">
+      <form onSubmit={handleSubmit} className="reservation-form">
+        <h2>Reserve this car</h2>
+        <input className="automatic" type="text" name="user_id" value={currentUser.name} readOnly />
+        { car ? <input className="automatic" type="text" name="car_id" value={car.name} readOnly />
+          : (
+            <select name="car_id" value={formData.car} onChange={handleInputChange}>
+              <option disabled value="Select a car">Select a Car</option>
+              {cars?.map((car) => (
+                <option key={car.id} value={car.id}>{car.name}</option>
+              ))}
+            </select>
+          )}
+        <select name="city" value={formData.city} onChange={handleInputChange}>
+          <option value="">Select City</option>
+          <option value="San Francisco">San Francisco</option>
+          <option value="Los Angeles">Los Angeles</option>
+          <option value="New York">New York</option>
+          <option value="Chicago">Chicago</option>
+        </select>
+        <input type="date" name="date" value={formData.date} onChange={handleInputChange} placeholder="Date" />
+        <button className="reservation-btn" type="submit">Add Reservation</button>
+      </form>
+    </div>
   );
 }
 
